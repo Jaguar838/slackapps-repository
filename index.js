cbb3a53189b90a8ec414df13d136b93c2839debb
BoberLob <75248620+Jaguar838@users.noreply.github.com>
2021-10-02 01:12:48 +0300

    add LogLevel

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const { App } = require("@slack/bolt");
 const { config } = require("dotenv");
+const LogLevel = require("loglevel");
 
 config();
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  // LogLevel can be imported and used to make debugging simpler
+  logLevel: LogLevel.debug,
 });
 
-// // Reverse all messages the app can hear
+// Reverse all messages the app can hear
 app.message(async ({ message, say }) => {
   const reversedText = [...message.text].reverse().join("");
   await say(reversedText);
